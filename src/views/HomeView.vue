dcd080765a1fe788eebe6470b0f52ebe64662e7d
renascm <renatochagas.m@gmail.com>
2025-05-04 19:34:40 -0300

    FEAT: add tailwind and style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,38 +20,37 @@ const postFilm = async () => {
 const patchFilm = async (filmData) => {
   await filmsStore.patchFilm(filmData);
 };
-
-
-
 </script>
 
 <template>
-  <div>
-    <div class="tittleWatch">
-      <h1>Filmes Para ver</h1>
-    </div>
-
-    <div class="toWatchList">
+  <div class="flex flex-col w-screen h-screen bg-black text-white gap-4">
+    <div class="flex flex-col items-center  border-3" style="padding: 5%">
       <div>
-        <input type="text" placeholder="adicionar filme" v-model="newFilmTitle" />
-
-        <button class="addFilm" @click="postFilm">Enter</button>
+        <h1>Para ver</h1>
       </div>
 
-      <div v-for="film in filmsStore.state.films" :key="film.id">
-        {{ film.Title }}
-        <button @click="patchFilm(film)">Visto</button>
+      <div class="flex flex-col gap-4">
+        <div class="flex gap-2">
+          <input class="rounded-2xl border-1 placeholder:pl-[5%]" type="text" placeholder="adicionar filme" v-model="newFilmTitle" />
+          <button class="px-3  rounded-2xl border-1" @click="postFilm">Enter</button>
+        </div>
+
+        <div class="flex justify-center gap-4 " v-for="film in filmsStore.state.films" :key="film.id">
+          {{ film.Title }}
+          <button @click="patchFilm(film)">Visto</button>
+        </div>
       </div>
     </div>
-
     <div>
-      <div>
-        <h1>Filmes assistindo</h1>
-      </div>
+      <div class="flex flex-col items-center  border-3" style="padding: 5%">
+        <div>
+          <h1 class="text-red-500" >Filmes assistidos</h1>
+        </div>
 
-      <div>
-        <div v-for="watched in filmsStore.state.watchedFilms" :key="watched.id">
-          {{ watched.Title }}
+        <div>
+          <div v-for="watched in filmsStore.state.watchedFilms" :key="watched.id">
+            {{ watched.Title }}
+          </div>
         </div>
       </div>
     </div>
